7bf4e0d9bf2f516cd593c32ca6c9b45c994c0ea9
nazuna <nazuna@nazuna.kr>
2019-02-28 01:48:36 +0900

    Add Function chkVirtualIP. Add auto filling ip in formshow.

--- mainFrm.pas
+++ mainFrm.pas
@@ -11,7 +11,7 @@ uses
   FireDAC.Stan.Option, FireDAC.Stan.Param, FireDAC.Stan.Error, FireDAC.DatS,
   FireDAC.Phys.Intf, FireDAC.DApt.Intf, Data.DB, FireDAC.Comp.DataSet,
   FireDAC.Comp.Client, REST.Response.Adapter, REST.Client, Data.Bind.Components,
-  Data.Bind.ObjectScope;
+  Data.Bind.ObjectScope, ipwcore, ipwipinfo;
 
 type
   TForm2 = class(TForm)
@@ -31,6 +31,8 @@ type
     RESTResponse1: TRESTResponse;
     RESTResponseDataSetAdapter1: TRESTResponseDataSetAdapter;
     FDMemTable1: TFDMemTable;
+    ipwIPInfo1: TipwIPInfo;
+    procedure FormShow(Sender: TObject);
   private
     { Private declarations }
   public
@@ -44,6 +46,44 @@ implementation
 
 {$R *.fmx}
 
+function chkVirtualIP(const AValue:string):boolean;
+var
+  sList:TStringList;
+  ipList: array[0..3] of integer;
+begin
+  sList:=TStringList.Create;
+  try
+    sList.Delimiter:='.';
+    sList.DelimitedText:=AValue;
 
+    ipList[0] :=  strtoint(sList[0]);
+    ipList[1] :=  strtoint(sList[1]);
+    ipList[2] :=  strtoint(sList[2]);
+    ipList[3] :=  strtoint(sList[3]);
+  finally
+    sList.Free;
+  end;
+
+  Result:= false;
+  case ipList[0] of
+    10,127: Result:=true;
+    169: if ipList[1] = 254 then Result:= true;
+    172: if (ipList[1] > 15) and (ipList[1] < 31) then Result:= true;
+    192: if ipList[1] = 168 then Result:= true;
+  end;
+end;
+
+procedure TForm2.FormShow(Sender: TObject);
+var I:integer;
+begin
+  for I := 0 to ipwIPInfo1.AdapterCount-1 do
+  begin
+    ipwIPInfo1.AdapterIndex := I;
+    if chkVirtualIP(ipwIPInfo1.AdapterIPAddress) then Continue;
+
+    edtIPorAS.Text := ipwIPInfo1.AdapterIPAddress;
+    Break;
+  end;
+end;
 
 end.
